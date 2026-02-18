311195ce50984d93d36737886ef16fa4749ca35c
Ryan M <ryanthemok@gmail.com>
2026-02-17 20:51:55 -0500

    formatting

--- src/lib/charts.ts
+++ src/lib/charts.ts
@@ -190,7 +190,7 @@ export function updateFooterSlogan(dataMap: Record<string, any>): void {
 	const tooltipText = `Last updated: ${lastUpdated}`;
 	const toastDuration = 5000;
 	const toastLongDuration = toastDuration * 3;
-	const toastRefreshText = "Requesting data refresh";
+	const toastRefreshText = "📡 Refreshing data";
 
 	if (footerSlogan && numBikesFixed && numVolunteers && numVisits) {
 		const totalBikes = numBikesFixed + numVolunteers;
