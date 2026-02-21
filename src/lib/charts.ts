adc8c3f1e63a60afe7b6556633d32309ca6c4dba
Ryan M <ryanthemok@gmail.com>
2026-02-21 14:15:04 -0500

    formatting

--- src/lib/charts.ts
+++ src/lib/charts.ts
@@ -187,7 +187,7 @@ export function updateFooterSlogan(dataMap: Record<string, any>): void {
 		lastUpdated = `${dayOfWeek}, ${shortDate} at ${time}`;
 	}
 
-	const tooltipText = `Last updated: ${lastUpdated}\nDays of Data: ${numDayOfData}`;
+	const tooltipText = `Last updated: ${lastUpdated}\nDays of data: ${numDayOfData}`;
 	const toastVolText = `${mvpVolStr}\n\n${tooltipText}`;
 	
 	const toastDuration = 5000;
